ae69cd88cca15ea3739c83be568a3cd9b264b931
Zacchaeus Bolaji <bolaji@prepclassng.com>
2019-11-24 23:06:02 +0100

    Add autoload for helper

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,12 @@
+<?php
+if(!function_exists('part_of_speech')) {
+    function part_of_speech($word) {
+        return (new \Djunehor\Grammar\Word())->getWordPartOfSpeech($word);
+    }
+}
+
+if(!function_exists('parts_of_speech')) {
+    function parts_of_speech() {
+        return (new \Djunehor\Grammar\Word())->getPartsOfSpeech();
+    }
+}
